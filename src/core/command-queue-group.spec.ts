a0f9292a35c8197e929f6c7f0b8a66e9f099c2d0
Darcy Rayner <darcy@two-bulls.com>
2019-03-08 16:29:39 -0500

    feat: create CommandQueueGroup class

---
+++ src/core/command-queue-group.spec.ts
@@ -0,0 +1,123 @@
+import { CommandQueueGroup } from './command-queue-group';
+import { CommandQueue } from './command-queue';
+import { waitForFrames } from './commands';
+
+describe('CommandQueueGroup', () => {
+  it('will call update on a created queue', () => {
+    const group = new CommandQueueGroup();
+    const newQueue = group.createQueue();
+    let called = false;
+
+    newQueue.enqueue(() => {
+      called = true;
+    });
+    group.update(0.0);
+    expect(called).toBeTruthy();
+  });
+  it('will call update on an added queue', () => {
+    const group = new CommandQueueGroup();
+    const queue = new CommandQueue();
+    group.addQueue(queue);
+    let called = false;
+
+    queue.enqueue(() => {
+      called = true;
+    });
+
+    group.update(0.0);
+    expect(called).toBeTruthy();
+  });
+  it("won't let an added queue be called twice", () => {
+    const group = new CommandQueueGroup();
+    const queue = group.createQueue();
+    group.addQueue(queue);
+    let calledCount = 0;
+
+    queue.enqueue(
+      () => {
+        calledCount++;
+      },
+      waitForFrames(1),
+      () => {
+        calledCount++;
+      }
+    );
+
+    group.update(0.0);
+    expect(calledCount).toBe(1);
+  });
+
+  it('will let a queue be removed immediately after it has been added', () => {
+    const group = new CommandQueueGroup();
+    const queue = group.createQueue();
+    group.removeQueue(queue);
+    let calledCount = 0;
+
+    queue.enqueue(() => {
+      calledCount++;
+    });
+
+    group.update(0.0);
+    expect(calledCount).toBe(0);
+  });
+
+  it('will let a queue be removed while the group is updating', () => {
+    const group = new CommandQueueGroup();
+    const queue = group.createQueue();
+    let calledCount = 0;
+
+    queue.enqueue(
+      () => {
+        calledCount++;
+        group.removeQueue(queue);
+      },
+      waitForFrames(1),
+      () => {
+        calledCount++;
+      }
+    );
+
+    group.update(0.0);
+    group.update(0.0);
+
+    expect(calledCount).toBe(1);
+  });
+
+  it('will let a queue be added while the group is updating', () => {
+    const group = new CommandQueueGroup();
+    const queue = group.createQueue();
+    let calledCount = 0;
+
+    queue.enqueue(() => {
+      calledCount++;
+      group.createQueue().enqueue(() => {
+        calledCount++;
+      });
+    });
+
+    group.update(0.0);
+
+    expect(calledCount).toBe(2);
+  });
+
+  it("will let a queue be added, after it's been removed and already ran once", () => {
+    const group = new CommandQueueGroup();
+    const queue = group.createQueue();
+    group.update(0);
+
+    let calledCount = 0;
+
+    queue.enqueue(() => {
+      calledCount++;
+      group.createQueue().enqueue(() => {
+        calledCount++;
+      });
+    });
+
+    group.removeQueue(queue);
+    group.addQueue(queue);
+    group.update(0);
+
+    expect(calledCount).toBe(2);
+  });
+});
---
+++ src/core/command-queue-group.ts
@@ -0,0 +1,74 @@
+import { CommandQueue } from './command-queue';
+
+export class CommandQueueGroup {
+  private newQueues: CommandQueue[] = [];
+  private queuesToRemove: CommandQueue[] = [];
+  private queues: CommandQueue[] = [];
+
+  /**
+   * Creates a queue, which will be updated by the group.
+   */
+  createQueue() {
+    const queue = new CommandQueue();
+    this.addQueue(queue);
+    return queue;
+  }
+
+  /**
+   * Adds the queue, to be updated by the group.
+   * @param queue The `CommandQueue` for the behaviour to update.
+   */
+  addQueue(queue: CommandQueue) {
+    const newQueuesIndex = this.newQueues.indexOf(queue);
+    const queuesIndex = this.queues.indexOf(queue);
+    const queuesToRemoveIndex = this.queuesToRemove.indexOf(queue);
+
+    if (newQueuesIndex === -1 && queuesIndex === -1) {
+      this.newQueues.push(queue);
+    }
+    if (queuesToRemoveIndex !== -1) {
+      this.queuesToRemove.splice(queuesToRemoveIndex, 1);
+    }
+  }
+
+  /**
+   * Removes a queue the group.
+   * @param queue The CommandQueue to remove. This queue should have been created, or added to this behaviour already.
+   */
+  removeQueue(queue: CommandQueue) {
+    const newQueuesIndex = this.newQueues.indexOf(queue);
+    const queuesIndex = this.queues.indexOf(queue);
+    const queuesToRemoveIndex = this.queuesToRemove.indexOf(queue);
+
+    if (queuesIndex !== -1 && queuesToRemoveIndex === -1) {
+      this.queuesToRemove.push(queue);
+    }
+    if (newQueuesIndex !== -1) {
+      this.newQueues.splice(newQueuesIndex, 1);
+    }
+  }
+
+  /**
+   * Updates all the queues in the group by the specified deltaTime.
+   * @param deltaTime The amount of time to update all the queues by.
+   */
+  update(deltaTime: number) {
+    const queuesToUpdate = [...this.queues];
+    do {
+      this.queues.push(...this.newQueues);
+      queuesToUpdate.push(...this.newQueues);
+
+      for (const queueToRemove of this.queuesToRemove) {
+        this.queues.splice(this.queues.indexOf(queueToRemove), 1);
+        queuesToUpdate.splice(queuesToUpdate.indexOf(queueToRemove), 1);
+      }
+
+      this.newQueues.length = 0;
+      this.queuesToRemove.length = 0;
+      const nextQueue = queuesToUpdate.pop();
+      if (nextQueue !== undefined) {
+        nextQueue.update(deltaTime);
+      }
+    } while (queuesToUpdate.length > 0 || this.newQueues.length > 0);
+  }
+}
