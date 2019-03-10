09373e1876fa087662675fdbac7adb5f47c5f551
Darcy Rayner <darcy@two-bulls.com>
2019-03-09 23:56:29 -0500

    chore: update export paths

--- src/core/commands/index.ts
+++ src/core/commands/index.ts
@@ -2,3 +2,4 @@ export * from './functional';
 export * from './number';
 export * from './object';
 export * from './common';
+export * from './color';
--- src/core/index.ts
+++ src/core/index.ts
@@ -1,3 +1,7 @@
 export * from './commands';
 export * from './ease';
 export * from './command-queue';
+export * from './command-scheduler';
+export * from './command-queue-group';
+export * from './ref';
+export * from './color-types';
