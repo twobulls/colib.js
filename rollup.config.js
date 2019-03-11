e7af4903af09afd031cfc9d7b3b788ba325fd18a
Darcy Rayner <darcy@two-bulls.com>
2019-03-10 22:08:21 -0400

    build: add configuration for browser build

--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,7 @@
 import typescript2 from 'rollup-plugin-typescript2';
+import { terser } from 'rollup-plugin-terser';
+import resolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
 
 import pkg from './package.json';
 export default [
@@ -25,5 +28,17 @@ export default [
     ],
     external: [...Object.keys(pkg.dependencies || {}), ...Object.keys(pkg.peerDependencies || {})],
     plugins: [typescript2()]
+  },
+  {
+    input: 'src/index.ts',
+    output: [
+      {
+        file: pkg.minified,
+        format: 'es',
+        sourcemap: false
+      }
+    ],
+    external: [...Object.keys(pkg.peerDependencies || {})],
+    plugins: [resolve(), commonjs(), typescript2(), terser()]
   }
 ];
